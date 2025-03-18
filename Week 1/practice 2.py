eeafc2c20f7c9f4a59fd550c2282224686924bf3
Isaac <isaacagbaku41@gmail.com>
2025-03-18 12:07:50 +0100

    practice2 complete

---
+++ Week 1/practice 2.py
@@ -0,0 +1,11 @@
+# python program to print strings and type
+str1 = "Hi my name is matthew. I am String"
+srt2 = 'Hi my name is Precious. I am also a String'
+
+ # displaying string str1 and its type
+ print(str1)
+ print(type(srt1)
+
+# displaying string str2 and its type
+ print(srt2)
+ print(type(srt2))
