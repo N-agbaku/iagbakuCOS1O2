ff7d7d66a25b563878aeae2d871d835c6fbc8db3
Isaac <isaacagbaku41@gmail.com>
2025-05-14 11:35:21 +0100

    practice 1

--- week2/practice 6.py
+++
@@ -1,9 +0,0 @@
-# Convert to int
-
-grade = int(70)
-gpa = int(4.9)
-cgpa = int("4")
-
-print(f"Grade = {grade}")
-print(f"GPA = {gpa}")
-print(f"CGPA = {cgpa}")
--- week2/practice 7.py
+++
@@ -1,9 +0,0 @@
-# Convert to float
-
-grade = float(97)
-gpa = float(5)
-cgpa = float("4.7")
-
-print(f"Grade = {grade}")
-print(f"GPA = {gpa}")
-print(f"CGPA = {cgpa}")
